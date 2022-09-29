5e2272dd7a512546a75244207a6f72fe57ca8976
Choo-Wooo <woochan780@gmail.com>
2022-09-29 10:08:08 +0900

    Week3+a use VGG16 model

--- Week3/Centralized_to_Federated/centralized.py
+++ Week3/Centralized_to_Federated/centralized.py
@@ -4,31 +4,32 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
+from torchvision import models, transforms
 import torchvision.transforms as transforms
 from torch import Tensor
 from torchvision.datasets import CIFAR10
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-class Net(nn.Module):
-
-    def __init__(self) -> None:
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+# class Net(nn.Module):
+#
+#     def __init__(self) -> None:
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 6, 5)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv2 = nn.Conv2d(6, 16, 5)
+#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.fc3 = nn.Linear(84, 10)
+#
+#     def forward(self, x: Tensor) -> Tensor:
+#         x = self.pool(F.relu(self.conv1(x)))
+#         x = self.pool(F.relu(self.conv2(x)))
+#         x = x.view(-1, 16 * 5 * 5)
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = self.fc3(x)
+#         return x
 
 
 def load_data():
@@ -90,12 +91,15 @@ def test(net, testloader, device):
     return loss, accuracy
 
 def main():
-    DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    DEVICE = torch.device("cpu")
     print("Centralized PyTorch training")
     print("Load data")
+    use_pretrained=True
     trainloader, testloader, _ = load_data()
     print("Start training")
-    net=Net().to(DEVICE)
+    net=models.vgg16(pretrained=use_pretrained)
+    net.eval()
     train(net=net, trainloader=trainloader, epochs=2, device=DEVICE)
     print("Evaluate model")
     loss, accuracy = test(net=net, testloader=testloader, device=DEVICE)
